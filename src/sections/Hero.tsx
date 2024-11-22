fa59f08651ac8adc0a4cd378862e64e91d57efbf
chrismazi <119514447+chrismazi@users.noreply.github.com>
2024-11-22 22:23:07 +0200

    Update Hero.tsx

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -4,10 +4,9 @@ import { Button } from "@/components/Button";
 import underlineImage from "@/assets/images/underline.svg?url";
 import Loader from "@/assets/images/loader.svg";
 import { Orbit } from "@/components/Orbit";
-import {Planet} from '@/components/Planet';
+import { Planet } from "@/components/Planet";
 import { SectionBorder } from "@/components/SectionBorder";
 import { SectionContent } from "@/components/SectionContent";
-
 
 export const Hero = () => {
   return (
@@ -15,9 +14,8 @@ export const Hero = () => {
       <div className="container">
         <SectionBorder>
           <SectionContent className="relative isolate  [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)]">
-          
             <div className="absolute -z-10 inset-0 bg-[radial-gradient(circle_farthest-corner,var(--color-fuchsia-900)_50%,var(--color-indigo-900)_75%,transparent)] [mask-image:radial-gradient(circle_farthest-side,black,transparent)]"></div>
-            
+
             {/* Updated Orbit Section */}
             <div className="absolute inset-0 -z-10">
               <div className="absolute-center">
@@ -42,20 +40,20 @@ export const Hero = () => {
               <span className="relative inline-block">
                 <span className="relative z-10">AI4Impact</span>
                 <span
-  className="absolute w-full h-[11px] md:h-[14px] lg:h-[16px] left-0 top-full translate-y-[4px]"
-  style={{
-    WebkitMaskImage: `url(${underlineImage.src})`,
-    WebkitMaskSize: "contain",
-    WebkitMaskPosition: "center",
-    WebkitMaskRepeat: "no-repeat",
-    maskImage: `url(${underlineImage.src})`,
-    maskSize: "contain",
-    maskPosition: "center",
-    maskRepeat: "no-repeat",
-    background: "linear-gradient(to right, var(--color-amber-300), var(--color-teal-300), var(--color-violet-400), var(--color-fuchsia-400))", // Gradient colors
-  }}
-></span>
-
+                  className="absolute w-full h-[11px] md:h-[14px] lg:h-[16px] left-0 top-full translate-y-[4px]"
+                  style={{
+                    WebkitMaskImage: `url(${underlineImage.src})`,
+                    WebkitMaskSize: "contain",
+                    WebkitMaskPosition: "center",
+                    WebkitMaskRepeat: "no-repeat",
+                    maskImage: `url(${underlineImage.src})`,
+                    maskSize: "contain",
+                    maskPosition: "center",
+                    maskRepeat: "no-repeat",
+                    background:
+                      "linear-gradient(to right, var(--color-amber-300), var(--color-teal-300), var(--color-violet-400), var(--color-fuchsia-400))",
+                  }}
+                ></span>
               </span>
             </h1>
             <p className="text-center text-lg md:text-xl mt-8 max-w-3xl mx-auto">
@@ -69,42 +67,63 @@ export const Hero = () => {
               </Button>
             </div>
             <div className="relative isolate max-w-5xl mx-auto">
-            <div className="absolute  left-1/2 top-0">
-            <Planet size="lg" color="violet" className="-translate-x-[278px] -translate-y-[80px] rotate-135"/>
-            <Planet size="lg" color="violet" className="-translate-y-[176.9px] translate-x-[268px] -rotate-135"/>
-            <Planet size="sm" color="fuchsia" className="-translate-y-[270px] -translate-x-[450px] rotate-135"/>
-            <Planet size="md" color="teal" className="-translate-y-[270px] translate-x-[420px] -rotate-135"/>
-              
+              <div className="absolute left-1/2 top-0">
+                <Planet
+                  size="lg"
+                  color="violet"
+                  className="-translate-x-[278px] -translate-y-[80px] rotate-135"
+                />
+                <Planet
+                  size="lg"
+                  color="violet"
+                  className="-translate-y-[176.9px] translate-x-[268px] -rotate-135"
+                />
+                <Planet
+                  size="sm"
+                  color="fuchsia"
+                  className="-translate-y-[270px] -translate-x-[450px] rotate-135"
+                />
+                <Planet
+                  size="md"
+                  color="teal"
+                  className="-translate-y-[270px] translate-x-[420px] -rotate-135"
+                />
               </div>
-              <div className="absolute left-0 z-10 top-[30%] -translate-x-10 hidden lg:block"> 
-                <div className="bg-gray-800/70 backdrop-blur-md border border-gray-700 rounded-xl p-4 w-72">
-                  <div>What exactly is AI4Impact? I've heard about it and would love to know more! </div>
-                  <div className="text-right text-gray-400 text-sm font-semibold">1m ago</div>
-                </div>
-              </div>
-              <div className="absolute right-0 z-10 top-[50%] translate-x-10 hidden lg:block"> 
+              <div className="absolute left-0 z-10 top-[30%] -translate-x-10 hidden lg:block">
                 <div className="bg-gray-800/70 backdrop-blur-md border border-gray-700 rounded-xl p-4 w-72">
                   <div>
-                    <strong>BrainZ:</strong> AI4Impact is a platform where students collaborate to use the power of AI for social good with creative AI-driven solutions.
+                    What exactly is AI4Impact? I&apos;ve heard about it and would
+                    love to know more!
                   </div>
-                  <div className="text-right text-gray-400 text-sm font-semibold">Just now</div>
+                  <div className="text-right text-gray-400 text-sm font-semibold">
+                    1m ago
+                  </div>
                 </div>
               </div>
-            <div className="mt-20 rounded-2xl border-2 overflow-hidden border-gradient relative">
-              
-  <Image src={robotImg} alt="Robot image" />
-  <div className="absolute bottom-2 md:bottom-4 lg:bottom-1px left-1/2 -translate-x-1/2 flex justify-center w-full px-4">
-    <div className="bg-gray-950/80 flex justify-center items-center gap-4 px-4 py-2 rounded-2xl w-[320px] max-w-full">
-      <Loader className="text-violet-400" />
-      <div className="font-semibold text-xl text-gray-100 flex items-center">
-        AI is generating<span className="ml-1">...</span>
-      </div>
-    </div>
-    </div>
-  </div>
-</div>
-
-          
+              <div className="absolute right-0 z-10 top-[50%] translate-x-10 hidden lg:block">
+                <div className="bg-gray-800/70 backdrop-blur-md border border-gray-700 rounded-xl p-4 w-72">
+                  <div>
+                    <strong>BrainZ:</strong> AI4Impact is a platform where
+                    students collaborate to use the power of AI for social good
+                    with creative AI-driven solutions.
+                  </div>
+                  <div className="text-right text-gray-400 text-sm font-semibold">
+                    Just now
+                  </div>
+                </div>
+              </div>
+              <div className="mt-20 rounded-2xl border-2 overflow-hidden border-gradient relative">
+                <Image src={robotImg} alt="Robot image" />
+                <div className="absolute bottom-2 md:bottom-4 lg:bottom-1px left-1/2 -translate-x-1/2 flex justify-center w-full px-4">
+                  <div className="bg-gray-950/80 flex justify-center items-center gap-4 px-4 py-2 rounded-2xl w-[320px] max-w-full">
+                    <Loader className="text-violet-400" />
+                    <div className="font-semibold text-xl text-gray-100 flex items-center">
+                      AI is generating<span className="ml-1">...</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </SectionContent>
         </SectionBorder>
       </div>
